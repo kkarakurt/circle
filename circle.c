c507b9a9615f67edbfdf9d0f8bd423f5875a9c1a
Kağan Karakurt <124518547+kkarakurt@users.noreply.github.com>
2023-08-16 11:51:38 +0300

    O

--- circle.c
+++ circle.c
@@ -1,11 +1,10 @@
-
-      #include <unistd.h>
-
-    	  int main(){				
-	   int i=0;int j=1;				
-	 while(1){while(i<j){			
-	write(1,"a",1); i++; }			
-	 j++;write(1,"\n",1);			
-	   i=0;if(j-1==031)				
-	      return 0;}}				
-
+                              
+     #include <unistd.h>      
+                        	  
+    	  int main(){	      
+	   int i=0;int j=1;       
+	 while(1){while(i<j){     
+	write(1,"a",1); i++; }    
+	 j++;write(1,"\n",1);     
+	   i=0;if(j-1==031)       
+	      return 0;}}          
